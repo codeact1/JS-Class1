8aeca69066a537a6166c2de00b1beb676187329f
L.Robinson <110437490+codeact1@users.noreply.github.com>
2022-08-22 19:24:22 -0500

    Assigment1
    Variables in Amazon

---
+++ script.js
@@ -0,0 +1,39 @@
+var userName="Chaka";
+
+let age=89;
+let kids="8";
+let partnerName="Tim";
+let geoLocation="Jacksonville";
+let jobTitle="Freelancer";
+const taxes="11";
+
+console.log(userName);
+console.log(age);
+console.log(taxes);
+
+document.write(`
+<p>Name: ${userName} </p>
+<p>Age: ${age}</p>
+<p>Taxes: ${taxes}</p>
+`); 
+
+//Exercise 1//
+document.write(`
+<p>You will be a ${jobTitle} in ${geoLocation}, and married to ${partnerName} with ${kids} kids.</p>`);
+
+
+//Exercise 2//
+
+let email="dragonslayer1@gmail.com";
+let password="password";
+let country="US";
+let salary=1000;
+let annualSalary = salary*12;
+
+document.write(`
+<p>${userName}</p>
+<p>${email}</p>
+<p>${annualSalary}</p>
+`)
+
+
